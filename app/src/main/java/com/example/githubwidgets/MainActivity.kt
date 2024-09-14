598d9a25d49f26b6c8416a01d7cf560d8274428f
amkhrjee <aniruddha.mukherji@outlook.com>
2024-09-14 17:12:19 +0530

    all design done

--- app/src/main/java/com/example/githubwidgets/MainActivity.kt
+++ app/src/main/java/com/example/githubwidgets/MainActivity.kt
@@ -25,12 +25,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -47,7 +49,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendModeColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -102,7 +103,32 @@ fun HomeActivity(context: Context) {
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet { Text("hello") }
+            ModalDrawerSheet {
+                Text("GH Widgets v0.1.2", modifier = Modifier.padding(16.dp))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.code_24px),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(text = "View Source Code") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.person_24px),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(text = "About me") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+            }
         },
     ) {
         Scaffold(topBar = {
@@ -193,14 +219,9 @@ fun HomeActivity(context: Context) {
                 }
             }
 
-//            Test Button
 
             Column(Modifier.padding(contentPadding)) {
-//                Text("Your Widgets", style = MaterialTheme.typography.titleMedium)
-//                Spacer(Modifier.size(6.dp))
-//                Button({
-//                    addWidget(context)
-//                }) { Text("Pin Widget") }
+
                 Card(
                     colors = CardDefaults.cardColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -220,7 +241,7 @@ fun HomeActivity(context: Context) {
                                 style = MaterialTheme.typography.titleLarge,
                                 color = MaterialTheme.colorScheme.onPrimaryContainer
                             )
-                            Button({addWidget(context)}) {
+                            Button({ addWidget(context) }) {
                                 Text("Add")
                             }
                         }
